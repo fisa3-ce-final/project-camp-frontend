814fb4f9a07e7869b1b90855d21bfec3357fefbe
recoild <cajhyung@gmail.com>
2024-11-22 15:04:39 +0900

    feat: 마이페이지 대여 중인 물품 컴포넌트에서 idToken 전달 방식 개선

--- app/(main)/mypage/items/page.tsx
+++ app/(main)/mypage/items/page.tsx
@@ -3,11 +3,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/lib/auth-options";
 import MyItemsPage from "./my-items-page";
 
-interface MyRentalItemsProps {
-    idToken: string;
-}
-
-const MyRentalItems: FC<MyRentalItemsProps> = async () => {
+const MyRentalItems: FC = async () => {
     const session = await getServerSession(authOptions);
 
     return <MyItemsPage idToken={session?.user.id_token!} />;
